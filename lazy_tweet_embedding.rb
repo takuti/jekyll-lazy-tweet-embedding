61d0a226b6786d5a7cca021f603d07e46c180702
Takuya Kitazawa <k.takuti@gmail.com>
2016-07-01 13:59:02 +0900

    fix the inline twitter url converting

--- lazy_tweet_embedding.rb
+++ lazy_tweet_embedding.rb
@@ -4,7 +4,7 @@ require "json"
 # convert tweet url to embedding html
 def embedding_tweet(content)
   embedded_content = content
-  content.scan(/(https?:\/\/twitter\.com\/[a-zA-Z0-9_]+\/status\/([0-9]+)\/?)/).each do |url, id|
+  content.scan(/^(https?:\/\/twitter\.com\/[a-zA-Z0-9_]+\/status\/([0-9]+)\/?)$/).each do |url, id|
     tweet_json = open("https://api.twitter.com/1/statuses/oembed.json?id=#{id}").read
     tweet_html = JSON.parse(tweet_json, { :symbolize_names => true })[:html]
     embedded_content = embedded_content.gsub(/#{url}/, tweet_html)
